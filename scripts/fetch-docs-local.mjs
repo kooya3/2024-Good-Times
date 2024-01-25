8c9016efd40917da91a5b0ddd824dd1a160af5d0
Elyees Tatua <elyeestatua@gmail.com>
2024-01-25 23:34:34 +0300

    ops(scripts): Working on the fetch-docs-local mjs file type file

--- scripts/fetch-docs-local.mjs
+++ scripts/fetch-docs-local.mjs
@@ -2,3 +2,11 @@
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable no-useless-escape */
 import dotenv from 'dotenv'
+
+import fs from 'fs'
+import matter from 'gray-matter'
+import { serialize } from 'next-mdx-remote/serialize'
+import path from 'path'
+import remarkGfm from 'remark-gfm'
+
+dotenv.config()
