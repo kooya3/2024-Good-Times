eed334a7b06a82ded89f51a170516e519b82058a
Elyees Tatua <elyeestatua@gmail.com>
2024-01-25 23:52:46 +0300

    ops(scripts): Creating the fetchDocs async function

--- scripts/fetch-docs-local.mjs
+++ scripts/fetch-docs-local.mjs
@@ -2,7 +2,6 @@
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable no-useless-escape */
 import dotenv from 'dotenv'
-
 import fs from 'fs'
 import matter from 'gray-matter'
 import { serialize } from 'next-mdx-remote/serialize'
@@ -10,7 +9,6 @@ import path from 'path'
 import remarkGfm from 'remark-gfm'
 
 dotenv.config()
-
 
 const __dirname = path.resolve()
 
@@ -21,12 +19,11 @@ const docsDirectory = process.env.DOCS_DIR
 console.log(`Fetching docs from: ${docsDirectory}`)
 
 function slugify(string) {
-    const a = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;'
-    const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz------'
-    const p = new RegExp(a.split('').join('|'), 'g')
-  
+  const a = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;'
+  const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz------'
+  const p = new RegExp(a.split('').join('|'), 'g')
 
-    return string
+  return string
     .toString()
     .toLowerCase()
     .replace(/\s+/g, '-') // Replace spaces with -
@@ -39,59 +36,104 @@ function slugify(string) {
 }
 
 const topicOrder = [
-    'Getting-Started',
-    'Configuration',
-    'Database',
-    'Fields',
-    'Admin',
-    'Rich-Text',
-    'Live-Preview',
-    'Access-Control',
-    'Hooks',
-    'Authentication',
-    'Versions',
-    'Upload',
-    'GraphQL',
-    'REST-API',
-    'Local-API',
-    'Queries',
-    'Production',
-    'Email',
-    'TypeScript',
-    'Plugins',
-    'Integrations',
-    'Cloud',
-  ]
+  'Getting-Started',
+  'Configuration',
+  'Database',
+  'Fields',
+  'Admin',
+  'Rich-Text',
+  'Live-Preview',
+  'Access-Control',
+  'Hooks',
+  'Authentication',
+  'Versions',
+  'Upload',
+  'GraphQL',
+  'REST-API',
+  'Local-API',
+  'Queries',
+  'Production',
+  'Email',
+  'TypeScript',
+  'Plugins',
+  'Integrations',
+  'Cloud',
+]
 
-  async function getHeadings(source) {
-    const headingLines = source.split('\n').filter(line => {
-      return line.match(/^#{1,3}\s.+/gm)
-    })
-  
-    return headingLines.map(raw => {
-      const text = raw.replace(/^###*\s/, '')
-      const level = raw.slice(0, 3) === '###' ? 3 : 2
-      return { text, level, id: slugify(text) }
-    })
-  }
+async function getHeadings(source) {
+  const headingLines = source.split('\n').filter(line => {
+    return line.match(/^#{1,3}\s.+/gm)
+  })
 
+  return headingLines.map(raw => {
+    const text = raw.replace(/^###*\s/, '')
+    const level = raw.slice(0, 3) === '###' ? 3 : 2
+    return { text, level, id: slugify(text) }
+  })
+}
 
 const fetchDocs = async () => {
-    const topics = await Promise.all(
-      topicOrder.map(async unsanitizedTopicSlug => {
-        const topicSlug = unsanitizedTopicSlug.toLowerCase()
-   
-        const topicDirectory = path.join(docsDirectory, `./${topicSlug}`)
-        const docSlugs = fs.readdirSync(topicDirectory)
-  
-        const parsedDocs = await Promise.all(
-          docSlugs.map(async docFilename => {
-            try {
-              const rawDoc = fs.readFileSync(
-                `${docsDirectory}/${topicSlug.toLowerCase()}/${docFilename}`,
-                'utf8',
-              )
+  const topics = await Promise.all(
+    topicOrder.map(async unsanitizedTopicSlug => {
+      const topicSlug = unsanitizedTopicSlug.toLowerCase()
 
+      const topicDirectory = path.join(docsDirectory, `./${topicSlug}`)
+      const docSlugs = fs.readdirSync(topicDirectory)
 
+      const parsedDocs = await Promise.all(
+        docSlugs.map(async docFilename => {
+          try {
+            const rawDoc = fs.readFileSync(
+              `${docsDirectory}/${topicSlug.toLowerCase()}/${docFilename}`,
+              'utf8',
+            )
 
             const parsedDoc = matter(rawDoc)
+
+            const doc = {
+              content: await serialize(parsedDoc.content, {
+                mdxOptions: {
+                  remarkPlugins: [remarkGfm],
+                },
+              }),
+              title: parsedDoc.data.title,
+              slug: docFilename.replace('.mdx', ''),
+              label: parsedDoc.data.label,
+              order: parsedDoc.data.order,
+              desc: parsedDoc.data.desc || '',
+              keywords: parsedDoc.data.keywords || '',
+              headings: await getHeadings(parsedDoc.content),
+            }
+
+            return doc
+          } catch (err) {
+            const msg = err instanceof Error ? err.message : err || 'Unknown error'
+            console.error(`Error fetching ${docFilename}: ${msg}`) // eslint-disable-line no-console
+          }
+        }),
+      )
+
+      const topic = {
+        slug: unsanitizedTopicSlug,
+        docs: parsedDocs.filter(Boolean).sort((a, b) => a.order - b.order),
+      }
+
+      return topic
+    }),
+  )
+
+  const data = JSON.stringify(topics, null, 2)
+
+  const docsFilename = path.resolve(__dirname, './src/app/docs.json')
+
+  fs.writeFile(docsFilename, data, err => {
+    if (err) {
+      console.error(err)
+    } else {
+      console.log(`Docs successfully written to ${docsFilename}`)
+    }
+    process.exit(0)
+  })
+}
+
+fetchDocs()
