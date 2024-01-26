9279f546c695c7decbf90ab456bcbda8ed5a0ba7
Elyees Tatua <elyeestatua@gmail.com>
2024-01-26 23:59:51 +0300

    ops(scripts): Updating the fetch- docs.mjs

--- scripts/fetch-docs.mjs
+++ scripts/fetch-docs.mjs
@@ -18,12 +18,11 @@ const decodeBase64 = string => {
 }
 
 function slugify(string) {
-    const a = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;'
-    const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz------'
-    const p = new RegExp(a.split('').join('|'), 'g')
-  
+  const a = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;'
+  const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz------'
+  const p = new RegExp(a.split('').join('|'), 'g')
 
-    return string
+  return string
     .toString()
     .toLowerCase()
     .replace(/\s+/g, '-') // Replace spaces with -
@@ -63,19 +62,91 @@ const topicOrder = [
 ]
 
 const headers = {
-    Accept: 'application/vnd.github.v3+json.html',
-    Authorization: `token ${process.env.GITHUB_ACCESS_TOKEN}`,
-  }
-  
+  Accept: 'application/vnd.github.v3+json.html',
+  Authorization: `token ${process.env.GITHUB_ACCESS_TOKEN}`,
+}
+
 async function getHeadings(source) {
-    const headingLines = source.split('\n').filter(line => {
-      return line.match(/^#{1,3}\s.+/gm)
-    })
-  
-    return headingLines.map(raw => {
-      const text = raw.replace(/^###*\s/, '')
-      const level = raw.slice(0, 3) === '###' ? 3 : 2
-      return { text, level, id: slugify(text) }
-    })
+  const headingLines = source.split('\n').filter(line => {
+    return line.match(/^#{1,3}\s.+/gm)
+  })
+
+  return headingLines.map(raw => {
+    const text = raw.replace(/^###*\s/, '')
+    const level = raw.slice(0, 3) === '###' ? 3 : 2
+    return { text, level, id: slugify(text) }
+  })
+}
+
+const fetchDocs = async () => {
+  if (!process.env.GITHUB_ACCESS_TOKEN) {
+    console.log('No GitHub access token found - skipping docs retrieval') // eslint-disable-line no-console
+    process.exit(0)
   }
-  
+
+  const topics = await Promise.all(
+    topicOrder.map(async unsanitizedTopicSlug => {
+      const topicSlug = unsanitizedTopicSlug.toLowerCase()
+
+      const docs = await fetch(`${githubAPI}/contents/docs/${topicSlug}`, {
+        headers,
+      }).then(res => res.json())
+
+      const docFilenames = docs.map(({ name }) => name)
+
+      const parsedDocs = await Promise.all(
+        docFilenames.map(async docFilename => {
+          try {
+            const json = await fetch(`${githubAPI}/contents/docs/${topicSlug}/${docFilename}`, {
+              headers,
+            }).then(res => res.json())
+
+            const parsedDoc = matter(decodeBase64(json.content))
+
+            const doc = {
+              content: await serialize(parsedDoc.content, {
+                mdxOptions: {
+                  remarkPlugins: [remarkGfm],
+                },
+              }),
+              title: parsedDoc.data.title,
+              slug: docFilename.replace('.mdx', ''),
+              label: parsedDoc.data.label,
+              order: parsedDoc.data.order,
+              desc: parsedDoc.data.desc || '',
+              keywords: parsedDoc.data.keywords || '',
+              headings: await getHeadings(parsedDoc.content),
+            }
+
+            return doc
+          } catch (err) {
+            const msg = err instanceof Error ? err.message : err || 'Unknown error'
+            console.error(`Error fetching ${docFilename}: ${msg}`) // eslint-disable-line no-console
+          }
+        }),
+      )
+
+      const topic = {
+        slug: unsanitizedTopicSlug,
+        docs: parsedDocs.filter(Boolean).sort((a, b) => a.order - b.order),
+      }
+
+      return topic
+    }),
+  )
+
+  const data = JSON.stringify(topics, null, 2)
+
+  const docsFilename = path.resolve(__dirname, './src/app/docs.json')
+
+  fs.writeFile(docsFilename, data, err => {
+    if (err) {
+      console.error(err) // eslint-disable-line no-console
+    } else {
+      console.log(`Docs successfully written to ${docsFilename}`) // eslint-disable-line no-console
+    }
+    process.exit(0)
+  })
+}
+
+fetchDocs()
