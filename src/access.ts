6e364cbe84c00ad6dacad34e8209aaf802a684de
Elyees Tatua <elyeestatua@gmail.com>
2024-01-28 22:56:17 +0300

    ops(src): Setting the access file's unique ID's

--- src/access.ts
+++ src/access.ts
@@ -34,3 +34,10 @@ export const checkRole = (allRoles: User['roles'], user: User): boolean => {
         ? project?.team.id
         : project?.team
   
+        if (!projectTeamID) return false
+
+        const isTeamOwner = userTeams.find(({ team, roles }) => {
+          const userTeamID = typeof team === 'object' && 'id' in team ? team.id : team
+          const userIsOnTeam = userTeamID === projectTeamID
+          return userIsOnTeam && (roles || []).includes('owner')
+        })
