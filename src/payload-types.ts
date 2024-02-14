68bf86504400c1265af27b462fce7a762cd08ca1
Elyees Tatua <elyeestatua@gmail.com>
2024-02-14 23:12:22 +0300

    ops(src): Working on defining the payload-types interface casestudy

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -809,5 +809,37 @@ export interface CaseStudy {
                                 }[]
                               | null;
                           };
+                          layout: (
+                            | {
+                                ctaFields: {
+                                  richText: {
+                                    [k: string]: unknown;
+                                  }[];
+                                  feature: 'none' | 'cpa';
+                                  links?:
+                                    | {
+                                        link: {
+                                          type?: ('reference' | 'custom') | null;
+                                          newTab?: boolean | null;
+                                          reference?:
+                                            | ({
+                                                relationTo: 'pages';
+                                                value: string | Page;
+                                              } | null)
+                                            | ({
+                                                relationTo: 'posts';
+                                                value: string | Post;
+                                              } | null)
+                                            | ({
+                                                relationTo: 'case-studies';
+                                                value: string | CaseStudy;
+                                              } | null);
+                                          url?: string | null;
+                                          label: string;
+                                        };
+                                        id?: string | null;
+                                      }[]
+                                    | null;
+                                };
                             
                     
